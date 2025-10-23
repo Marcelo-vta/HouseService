aecaf4b739c412e101ac3d902cb62c31f71ec491
marcelo-vta <marcelovta@al.insper.edu.br>
2025-10-23 13:17:50 -0300

    mouse based rotation

--- Assets/Scripts/playerRotation.cs
+++ Assets/Scripts/playerRotation.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class playerRotation : MonoBehaviour
 {
+    private Vector2 mousePos;
+    public Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,18 @@ public class playerRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Input.mousePosition;
+
+        mousePos.x -= Screen.width/2;
+        mousePos.y -= Screen.height / 2;
+
+        mousePos.x /= Screen.width / 2;
+        mousePos.y /= Screen.height / 2;
+
+        animator.SetFloat("mouseX", mousePos.x);
+        animator.SetFloat("mouseY", mousePos.y);
+
+
+        Debug.Log("Mouse Screen Position (Legacy): " + mousePos);
     }
 }
